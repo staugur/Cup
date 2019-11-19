e4be10fc546796932da93207bdf79df0eaec1a23
Mr.tao <staugur@saintic.com>
2019-11-19 14:01:30 +0800

    update my status

--- js/config.js
+++ js/config.js
@@ -1,19 +1,15 @@
 // array of Monitor-specific API keys or Main API key to list all monitors
 var __apiKeys = [
-	'm776858319-7c829d2c3e250f85776f588e', //Community
-	'm776858325-1ddf7c7e98ce56609325db3b', //Document
-	'm782409254-3c4b19cd3f7082f404df494e', //Me
-	'm776858407-a704cd548fb5c15db5b625f9', //Update A
-	'm778126978-24e6f1ce275c2518bad1e925', //Update B
-	'm777157148-a67f71b8a807c54b8ce92c9d', //Update deliver A
-	'm777157137-ce781489d1af15253ca3f0c1', //Update deliver B
-	'm783440738-5d66a6c6630333b0268ee437', //Update deliver C
-	'm776858320-7b44f9ac136f69ac3e356e8e', //Auth A
-	'm776858321-486827042ed7a7f7b8952a05', //Auth B
-	'm778734196-a51947f061c5db10cee048f7', //Auth C
-	'm776858323-0bdafb9e9a71e3305c27c1af', //Auth D
-	'm777190463-ac0ab733e1b29c92b7de0354', //Auth E
+    'm783666069-da3589b3a86727af0e25d472', //www
+    'm783682631-e00fac9bd90a5b38f1b381e6',//www.ping
+    'm783666089-f7d727c214e269498afa048a',//shorturl
+    'm783666097-949d4c9036be0d8a33f521c9',//passport
+    'm782963190-1e2b4ef89d0def953120c64d',//open
+    'm783666080-bb33cbf134698f37919b60df',//docs
+    'm783666077-372e6852c68481c8cae61ff8',//blog
+    'm783836067-f6aa67526bc991b351465fd8',//isso
+    'm783836062-f51a0aef5ec180679a60c08a',//rtfd
 ];
 
 // refresh interval (in seconds)
-var __refresh = 180;
+var __refresh = 60;
