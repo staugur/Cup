9bdad2342b5e3b3c8f5757471fa09552b9a802b2
Mr.tao <staugur@saintic.com>
2019-11-21 18:01:45 +0800

    update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 // array of Monitor-specific API keys or Main API key to list all monitors
 var __apiKeys = [
     'm783666069-da3589b3a86727af0e25d472', //www
-    'm783682631-e00fac9bd90a5b38f1b381e6',//www.ping
+    'm783852340-42c531ce1d35e046889aa51b',//static
     'm783666089-f7d727c214e269498afa048a',//shorturl
     'm783666097-949d4c9036be0d8a33f521c9',//passport
     'm782963190-1e2b4ef89d0def953120c64d',//open
@@ -12,7 +12,7 @@ var __apiKeys = [
 ];
 
 // refresh interval (in seconds)
-var __refresh = 60;
+var __refresh = 30;
 
 // your site name
 var __siteName = "SaintIC";
