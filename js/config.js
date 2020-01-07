277ac460df3e194eec69c5531b49350488d0518b
Hiroshi.tao <staugur@saintic.com>
2020-01-07 09:37:52 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -9,6 +9,7 @@ var __apiKeys = [
     'm783666077-372e6852c68481c8cae61ff8',//blog
     'm783836067-f6aa67526bc991b351465fd8',//isso
     'm783836062-f51a0aef5ec180679a60c08a',//rtfd
+    'm784104287-8f2cf09927757d4ce07815ec',//picbed
 ];
 
 // refresh interval (in seconds)
