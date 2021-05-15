5458f4313752f953cd4939a6ed86334d12ad30dd
Hiroshi.tao <staugur@saintic.com>
2021-05-15 13:00:29 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -8,8 +8,10 @@ var __apiKeys = [
     'm783666080-bb33cbf134698f37919b60df',//docs
     'm783666077-372e6852c68481c8cae61ff8',//blog
     'm783836067-f6aa67526bc991b351465fd8',//isso
-    'm783836062-f51a0aef5ec180679a60c08a',//rtfd
+    'm783836062-f51a0aef5ec180679a60c08a',//rtfd-server
+    'm788143922-fcc23259f25cb83bc5ba4abb',//rtfd-app
     'm784104287-8f2cf09927757d4ce07815ec',//picbed
+    'm788143930-06672a9cb78f977ab64e31d5',//fairyla
 ];
 
 // refresh interval (in seconds)
